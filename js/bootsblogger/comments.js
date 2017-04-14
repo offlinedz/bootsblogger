f4275cfdc949fccc58cf122d4f26fd50455d0f7a
Igoy <igoynawamreh@gmail.com>
2017-04-14 10:13:29 +0700

    Update comments: use new data and redesign.

--- js/bootsblogger/comments.js
+++ js/bootsblogger/comments.js
@@ -23,7 +23,7 @@
     })
 
 
-    // Add/remove `.active` for form container
+    // Toogle `.active` for `.comment-form-container-original` and `.comment-form-container-reply`
 
     $('.comment-form-container-original').addClass('active')
 
@@ -38,7 +38,7 @@
     })
 
 
-    // Add/remove `.show` for `add comment` and `cancel reply` wrapper
+    // Toggle `.show` for `.comment-add-wrapper` and `.comment-cancel-wrapper`
 
     $('.js-comment-action').on('click', function () {
       $('.js-comment-action').removeClass('active')
@@ -63,63 +63,20 @@
 
     // Loading animation
 
-    $('.comment-form').addClass('comment-form-loading')
+    $('.comment-form').addClass('js-comment-form-loading')
 
     $('#comment-editor').on('load', function () {
-      $('.comment-form').removeClass('comment-form-loading')
+      $('.comment-form').removeClass('js-comment-form-loading')
     })
 
     $('.js-comment-action').click(function () {
-      $('.comment-form').addClass('comment-form-loading')
+      $('.comment-form').addClass('js-comment-form-loading')
 
       $('#comment-editor').on('load', function () {
-        $('.comment-form').removeClass('comment-form-loading')
+        $('.comment-form').removeClass('js-comment-form-loading')
       })
     })
-
-
-    // Avatar
-
-    // Resize
-    if (document.getElementById('comments') !== null) {
-      var commentResizeAvatar = document.getElementById('comments')
-      var commentAvatar = commentResizeAvatar.getElementsByTagName('span')
-      if (commentAvatar) {
-        for (var i = 0; i < commentAvatar.length; i++) {
-          var s = commentAvatar.item(i).innerHTML
-          if (s.indexOf('style="') != -1) {
-            s = s.replace(/src="https:\/\/img1.blogblog.com\/img\/blank.gif"/, '')
-            s = s.replace(/src="http:\/\/img1.blogblog.com\/img\/blank.gif"/, '')
-            s = s.replace(/display: none/i, '')
-            s = s.replace(/longdesc=/i, 'src=')
-            s = s.replace(/\/s[0-9]+\//, '/s50/')
-            commentAvatar.item(i).innerHTML = s
-          }
-        }
-      }
-    }
-
-    // Replace blank image
-    $('#comments img[src="//img1.blogblog.com/img/blank.gif"]').attr('src', 'https://placehold.it/50/777/eee?text=?')
 
   })
 
 }(jQuery));
-
-
-// Comment editor iframe for reply form
-//
-// Original <http://www.dte.web.id/2013/01/membuat-fitur-komentar-berbalas.html>
-
-if (document.getElementById('comment-editor') !== null) {
-  var commentEditorIframeOriginal = document.getElementById('comment-editor').src.split('#')
-  function reply(id) {
-    var form = document.getElementById('comment-form')
-    var frame = document.getElementById('comment-editor')
-    var container = (id != 'cancel') ? document.getElementById('comment-form-container-reply-' + id) : document.getElementById('comment-form-container-original')
-    var part = commentEditorIframeOriginal
-
-    container.insertBefore(form, null)
-    frame.src = (id != 'cancel') ? part[0] + '&parentID=' + id + '#' + part[1] : part[0] + '#' + part[1]
-  }
-}
